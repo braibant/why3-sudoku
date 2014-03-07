03df31e2e12d04e58867e273b5fd08476856339f
Thomas Braibant <thomas.braibant@inria.fr>
2014-03-07 15:27:33 +0100

    End

--- sudoku/sudoku_Solve_valid_chunk_start_invariant_3.v
+++ sudoku/sudoku_Solve_valid_chunk_start_invariant_3.v
@@ -235,57 +235,6 @@ Axiom valid_chunk_ex2 : forall (g:(map.Map.map Z Z)) (i:Z) (j:Z) (x:Z)
   (valid_chunk (map.Map.set g i x) j start offsets)))).
 
 (* Why3 assumption *)
-Definition valid_chunks (g:(map.Map.map Z Z)) (i:Z): Prop := (valid_chunk g i
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 1%Z) 2%Z 2%Z) 3%Z 3%Z) 4%Z 4%Z) 5%Z 5%Z) 6%Z 6%Z) 7%Z
-  7%Z) 8%Z 8%Z) 9%Z 0%Z) 10%Z 1%Z) 11%Z 2%Z) 12%Z 3%Z) 13%Z 4%Z) 14%Z 5%Z)
-  15%Z 6%Z) 16%Z 7%Z) 17%Z 8%Z) 18%Z 0%Z) 19%Z 1%Z) 20%Z 2%Z) 21%Z 3%Z) 22%Z
-  4%Z) 23%Z 5%Z) 24%Z 6%Z) 25%Z 7%Z) 26%Z 8%Z) 27%Z 0%Z) 28%Z 1%Z) 29%Z 2%Z)
-  30%Z 3%Z) 31%Z 4%Z) 32%Z 5%Z) 33%Z 6%Z) 34%Z 7%Z) 35%Z 8%Z) 36%Z 0%Z) 37%Z
-  1%Z) 38%Z 2%Z) 39%Z 3%Z) 40%Z 4%Z) 41%Z 5%Z) 42%Z 6%Z) 43%Z 7%Z) 44%Z 8%Z)
-  45%Z 0%Z) 46%Z 1%Z) 47%Z 2%Z) 48%Z 3%Z) 49%Z 4%Z) 50%Z 5%Z) 51%Z 6%Z) 52%Z
-  7%Z) 53%Z 8%Z) 54%Z 0%Z) 55%Z 1%Z) 56%Z 2%Z) 57%Z 3%Z) 58%Z 4%Z) 59%Z 5%Z)
-  60%Z 6%Z) 61%Z 7%Z) 62%Z 8%Z) 63%Z 0%Z) 64%Z 1%Z) 65%Z 2%Z) 66%Z 3%Z) 67%Z
-  4%Z) 68%Z 5%Z) 69%Z 6%Z) 70%Z 7%Z) 71%Z 8%Z) 72%Z 0%Z) 73%Z 1%Z) 74%Z 2%Z)
-  75%Z 3%Z) 76%Z 4%Z) 77%Z 5%Z) 78%Z 6%Z) 79%Z 7%Z) 80%Z 8%Z)
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 9%Z) 2%Z 18%Z) 3%Z 27%Z) 4%Z 36%Z) 5%Z 45%Z) 6%Z 54%Z)
-  7%Z 63%Z) 8%Z 72%Z)) /\ ((valid_chunk g i
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 0%Z) 2%Z 0%Z) 3%Z 0%Z) 4%Z 0%Z) 5%Z 0%Z) 6%Z 0%Z) 7%Z
-  0%Z) 8%Z 0%Z) 9%Z 9%Z) 10%Z 9%Z) 11%Z 9%Z) 12%Z 9%Z) 13%Z 9%Z) 14%Z 9%Z)
-  15%Z 9%Z) 16%Z 9%Z) 17%Z 9%Z) 18%Z 18%Z) 19%Z 18%Z) 20%Z 18%Z) 21%Z 18%Z)
-  22%Z 18%Z) 23%Z 18%Z) 24%Z 18%Z) 25%Z 18%Z) 26%Z 18%Z) 27%Z 27%Z) 28%Z
-  27%Z) 29%Z 27%Z) 30%Z 27%Z) 31%Z 27%Z) 32%Z 27%Z) 33%Z 27%Z) 34%Z 27%Z)
-  35%Z 27%Z) 36%Z 36%Z) 37%Z 36%Z) 38%Z 36%Z) 39%Z 36%Z) 40%Z 36%Z) 41%Z
-  36%Z) 42%Z 36%Z) 43%Z 36%Z) 44%Z 36%Z) 45%Z 45%Z) 46%Z 45%Z) 47%Z 45%Z)
-  48%Z 45%Z) 49%Z 45%Z) 50%Z 45%Z) 51%Z 45%Z) 52%Z 45%Z) 53%Z 45%Z) 54%Z
-  54%Z) 55%Z 54%Z) 56%Z 54%Z) 57%Z 54%Z) 58%Z 54%Z) 59%Z 54%Z) 60%Z 54%Z)
-  61%Z 54%Z) 62%Z 54%Z) 63%Z 63%Z) 64%Z 63%Z) 65%Z 63%Z) 66%Z 63%Z) 67%Z
-  63%Z) 68%Z 63%Z) 69%Z 63%Z) 70%Z 63%Z) 71%Z 63%Z) 72%Z 72%Z) 73%Z 72%Z)
-  74%Z 72%Z) 75%Z 72%Z) 76%Z 72%Z) 77%Z 72%Z) 78%Z 72%Z) 79%Z 72%Z) 80%Z
-  72%Z)
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 1%Z) 2%Z 2%Z) 3%Z 3%Z) 4%Z 4%Z) 5%Z 5%Z) 6%Z 6%Z) 7%Z
-  7%Z) 8%Z 8%Z)) /\ (valid_chunk g i
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 0%Z) 2%Z 0%Z) 3%Z 3%Z) 4%Z 3%Z) 5%Z 3%Z) 6%Z 6%Z) 7%Z
-  6%Z) 8%Z 6%Z) 9%Z 0%Z) 10%Z 0%Z) 11%Z 0%Z) 12%Z 3%Z) 13%Z 3%Z) 14%Z 3%Z)
-  15%Z 6%Z) 16%Z 6%Z) 17%Z 6%Z) 18%Z 0%Z) 19%Z 0%Z) 20%Z 0%Z) 21%Z 3%Z) 22%Z
-  3%Z) 23%Z 3%Z) 24%Z 6%Z) 25%Z 6%Z) 26%Z 6%Z) 27%Z 27%Z) 28%Z 27%Z) 29%Z
-  27%Z) 30%Z 30%Z) 31%Z 30%Z) 32%Z 30%Z) 33%Z 33%Z) 34%Z 33%Z) 35%Z 33%Z)
-  36%Z 27%Z) 37%Z 27%Z) 38%Z 27%Z) 39%Z 30%Z) 40%Z 30%Z) 41%Z 30%Z) 42%Z
-  33%Z) 43%Z 33%Z) 44%Z 33%Z) 45%Z 27%Z) 46%Z 27%Z) 47%Z 27%Z) 48%Z 30%Z)
-  49%Z 30%Z) 50%Z 30%Z) 51%Z 33%Z) 52%Z 33%Z) 53%Z 33%Z) 54%Z 54%Z) 55%Z
-  54%Z) 56%Z 54%Z) 57%Z 57%Z) 58%Z 57%Z) 59%Z 57%Z) 60%Z 60%Z) 61%Z 60%Z)
-  62%Z 60%Z) 63%Z 54%Z) 64%Z 54%Z) 65%Z 54%Z) 66%Z 57%Z) 67%Z 57%Z) 68%Z
-  57%Z) 69%Z 60%Z) 70%Z 60%Z) 71%Z 60%Z) 72%Z 54%Z) 73%Z 54%Z) 74%Z 54%Z)
-  75%Z 57%Z) 76%Z 57%Z) 77%Z 57%Z) 78%Z 60%Z) 79%Z 60%Z) 80%Z 60%Z)
-  (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
-  Z Z)) 0%Z 0%Z) 1%Z 1%Z) 2%Z 2%Z) 3%Z 9%Z) 4%Z 10%Z) 5%Z 11%Z) 6%Z 18%Z) 7%Z
-  19%Z) 8%Z 20%Z))).
-
-(* Why3 assumption *)
 Definition diff (i:Z) (j:Z): Prop :=
   (~ ((map.Map.get (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.set (map.Map.const (-1%Z)%Z:(map.Map.map
   Z Z)) 0%Z 0%Z) 1%Z 0%Z) 2%Z 0%Z) 3%Z 3%Z) 4%Z 3%Z) 5%Z 3%Z) 6%Z 6%Z) 7%Z
@@ -371,6 +320,7 @@ Theorem valid_chunk_start_invariant : forall (g:(map.Map.map Z Z))
   (start:(map.Map.map Z Z)) (offsets:(map.Map.map Z Z)), forall (i:Z) (j:Z),
   (valid_chunk g i start offsets) -> (((map.Map.get start
   i) = (map.Map.get start j)) -> (valid_chunk g j start offsets)).
+(* Why3 intros g start offsets i j h1 h2. *)
 intros g start offsets i j h1 h2.
 unfold valid_chunk; setoid_rewrite <- h2.  apply h1. 
 Qed.
